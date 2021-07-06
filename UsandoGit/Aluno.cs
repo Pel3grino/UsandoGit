2263498b9301e6f90cd8d90c538906b3657fe098
pierre.romano <pierre.romano@vvssistemas.com.br>
2021-07-05 23:52:28 -0300

    ajuste

--- UsandoGit/Aluno.cs
+++ UsandoGit/Aluno.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UsandoGit.Interfaces;
 
 namespace UsandoGit
+
 {
-    class Aluno
+    public class Aluno:IAtividades
     {
+        public int DiasDeAtividades { get; set; }
+
+        public void Andar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Correr()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Nadar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Saltar()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
